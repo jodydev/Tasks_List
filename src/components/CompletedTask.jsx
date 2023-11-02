3b422a2a31574991f0fc784b18105a9abe083dc3
jodydev <jodyossino2001@gmail.com>
2023-11-02 19:48:23 +0100

    Commit-3

--- src/components/CompletedTask.jsx
+++ src/components/CompletedTask.jsx
@@ -22,7 +22,7 @@ function CompletedTask({ tasks }) {
             </div>
           </div>
         ) : (
-          completedTasks.map((task-2) => (
+          completedTasks.map((task) => (
             <div className="item rounded-3 my-3 mx-2" key={task.id}>
               <div className="container">
                 <div className="row">
